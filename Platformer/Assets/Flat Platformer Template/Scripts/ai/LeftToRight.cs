07be4c60303745e7c11fe2e46b95d2a8727ebc1c
Joshua Michael Daly <joshua.michael.daly@gmail.com>
2020-02-01 12:11:00 +0000

    Add basic Goomba enemy, blocks, and button/door

---
+++ Platformer/Assets/Flat Platformer Template/Scripts/ai/LeftToRight.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeftToRight : MonoBehaviour
+{
+    public float begin;
+    public float distance = 5;
+    public float speed = 5;
+    public int direction;
+
+    private bool changeDirection = false;
+    private bool stop = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        begin = transform.position.x;
+        direction = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stop)
+        {
+            return;
+        }
+
+        if (changeDirection)
+        {
+            changeDirection = false;
+            direction = -direction;
+        }
+        else if (transform.position.x > begin + distance)
+        {
+            direction = -1;
+        }
+        else
+        {
+            if (transform.position.x < begin - distance)
+            {
+                direction = 1;
+            }
+        }
+
+        transform.position = new Vector3(
+            transform.position.x + Time.deltaTime * speed * direction,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("josh");
+        }
+        else if (collision.gameObject.CompareTag("Block"))
+        {
+            changeDirection = true;
+        }
+        else if (collision.gameObject.CompareTag("Plant"))
+        {
+            stop = true;
+        }
+        else
+        {
+            // Do nothing
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Plant"))
+        {
+            stop = false;
+        }
+    }
+
+}
---
+++ Platformer/Assets/Flat Platformer Template/Scripts/objects/Button.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public string doorName;
+    private bool closed;
+
+    void Start()
+    {
+        closed = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("collider");
+        if (collider.gameObject.transform.parent.CompareTag("Player"))
+        {
+            if (closed)
+            {
+                GameObject door = GameObject.Find(doorName);
+                if (door)
+                {
+                    Destroy(door);
+                }
+                closed = false;
+            }
+        }
+        else
+        {
+            // Do nothing
+        }
+    }
+
+}
