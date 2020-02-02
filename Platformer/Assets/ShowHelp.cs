b281a918c4bafeb3ebdbada608c75c4843df9d97
Joshua Michael Daly <joshua.michael.daly@gmail.com>
2020-02-02 11:53:11 +0000

    added Help button

---
+++ Platformer/Assets/ShowHelp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHelp : MonoBehaviour
+{
+    public GameObject HelpImage;
+    void Start()
+    {
+        HelpImage.SetActive(false);
+    }
+
+    public void Click()
+    {
+        if(HelpImage.active == true)
+        {
+            HelpImage.SetActive(false);
+        }
+        else
+        {
+            HelpImage.SetActive(true);
+        }
+       
+    }
+
+    public void Hide()
+    {
+        HelpImage.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
